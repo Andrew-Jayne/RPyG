e6cc59c09241b842a5977cb8bc74d1d1c5c0b885
Andrew Jayne <andrew.jayne@cyberdefense.global>
2023-10-02 18:39:20 -0600

    logic updates

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-#Main.
-
+import random
 step = 0 
+health = 20
 
 while step < 100:
     step += 1
-    print(step)
+    encounter_check = random.randint(1,10)
+    if encounter_check == 5:
+        print("An Enemy was encountered!")
+        health -= 3
+        if health == 0:
+            print(f"you have fallen in combat at step {step}")
+            break
+    elif encounter_check == 1:
+        print("You Find an Inn and Rest")
+        health += 5
+    else:
+        print(step)
