fe2039a574b7ebc5f05b34b17ffef36a991b225f
Andrew Jayne <andrew.jayne@cyberdefense.global>
2023-10-03 22:09:52 -0600

    more upgrades and a heap of comments

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ while step < 100:
     encounter_check = random.randint(1,8)
     if encounter_check == 5:
         print("An Enemy was encountered!")
-        the_player.damage(3)
+        the_player.damage(5)
         print(f"Your Health is now {the_player.health}")
         if the_player.health == 0:
-            print(f"you have fallen in combat at step {step}")
+            print(f"{the_player.name} has fallen in combat at step {step}")
             break
     elif encounter_check == 1:
         print("You Find an Inn and Rest")
@@ -22,3 +22,19 @@ while step < 100:
         print(f"Your Health is now {the_player.health}")
     else:
         print(step)
+
+
+### Todo
+### Upgrade encounter system, to have more than 1 positive and 1 negative encounter
+
+### positive [Inn +5 HP, Tavern +3 HP]
+### Negative [Monster -5 HP, Bandit -3 HP]
+
+### Add Magicka & Stamina (no function just put them there)
+
+### add basic attributes (Str, Int, Agl) (No function yet Mostly a placeholder / prep for actor class)
+
+### Inventory System Same idea as attributes, no function but it's there (Gold, Health Potions)
+
+## Inv and Attrib would be classes nested into the player class (eh... we can just have them be part of the class directly for now)
+
--- player.py
+++ player.py
@@ -6,6 +6,14 @@ class Player:
 
     def damage(self, damage_amount:int):
         self.health -= damage_amount
+        if self.health == 0:
+            self.health = 1
+            print("You Narrowly Escape Death!")
+        elif self.health < 0:
+            self.health = 0
     
     def heal(self, heal_amount:int):
         self.health += heal_amount
+        if self.health > 20:
+            self.health = 20
+            print("You Are Fully Healed!")
