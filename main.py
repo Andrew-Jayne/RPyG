e606dd5c7b06019baebe265b00d898f5441a2d44
Andrew Jayne <andrew.jayne@cyberdefense.global>
2023-10-03 20:57:23 -0600

    upgraded health and damage systems

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import random
+from player import Player
+
+#Initialize Player
+the_player = Player("Protagonist", 20)
+
 step = 0 
-health = 20
 
 while step < 100:
     step += 1
-    encounter_check = random.randint(1,10)
+    encounter_check = random.randint(1,8)
     if encounter_check == 5:
         print("An Enemy was encountered!")
-        health -= 3
-        if health == 0:
+        the_player.damage(3)
+        print(f"Your Health is now {the_player.health}")
+        if the_player.health == 0:
             print(f"you have fallen in combat at step {step}")
             break
     elif encounter_check == 1:
         print("You Find an Inn and Rest")
-        health += 5
+        the_player.heal(5)
+        print(f"Your Health is now {the_player.health}")
     else:
         print(step)
