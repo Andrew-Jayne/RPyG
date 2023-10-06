c0da3a3eb81a57b31ae6178a538eb68fef425340
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2023-10-06 02:57:13 -0600

    implemented combat, potions use & buying, new enemies, buffed health, added player attack for stats

--- encounters.py
+++ encounters.py
@@ -36,16 +36,21 @@ class RestEncounters(Encounters):
     
     @classmethod
     def rest_encounter(self ,player_instance):
-        rest_chance = random.randint(0,2)
-        if rest_chance == 0:
+        rest_chance = random.randint(0,3)
+        if rest_chance == 0 or 1:
             player_instance.heal(3)
             print("You Find a Tavern and Rest for a short time")
-        elif rest_chance == 1:
+        elif rest_chance == 2:
             player_instance.heal(5)
             print("You Find an Inn and Rest for the evening")
-        elif rest_chance == 2:
+        elif rest_chance == 3:
             player_instance.heal(7)
             print("You Find the King's Vassal's Keep, and take a day to rest.")
+            while player_instance.potions > 5 and player_instance.gold != 0:
+                player_instance.gold -= 25
+                player_instance.potions += 1
+                print(f"You purchase a potion. You now have {player_instance.potions}")
+
 
             
 
@@ -58,17 +63,20 @@ class EnemyEncounters(Encounters):
     def enemy_encounter(self, player_instance):
         enemy_chance = random.randint(0,2)
         if enemy_chance == 0:
-            player_instance.damage(3)
-            small_enemy = Enemy(name="Feral Imp", health=10, strength=1, intellect=5, attack="fireball")
+            small_enemy = Enemy(name="Feral Imp", health=8, strength=1, intellect=3, attack="fireball")
             print(f"You encounter a {small_enemy.name}!")
+            enemy_instance = small_enemy
         elif enemy_chance == 1:
-            player_instance.damage(5)
-            medium_enemy = Enemy(name="Dire Wolf", health=20, strength=6, intellect=3, attack="Wolf Bite")
+            medium_enemy = Enemy(name="Dire Wolf", health=15, strength=5, intellect=3, attack="Wolf Bite")
             print(f"You encounter a {medium_enemy.name}!")
+            enemy_instance = medium_enemy
         elif enemy_chance == 2:
-            player_instance.damage(7)
-            large_enemy = Enemy(name="Cave Troll", health=40, strength=10, intellect=0, attack="Club Smash")
+            large_enemy = Enemy(name="Cave Troll", health=40, strength=10, intellect=1, attack="Club Smash")
             print(f"You encounter a {large_enemy.name}!")
+            enemy_instance = large_enemy
+
+        Combat.combat(player_instance, enemy_instance)
+
         
 
 class MysteryEncounters(Encounters):
