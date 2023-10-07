b0447cc7fc8c064d49f71138f95f16fd8931b114
Andrew-Jayne <273146696+Andrew-Jayne@users.noreply.github.com>
2023-10-06 21:27:06 -0600

    IDK but you can beat it while not being a god

--- encounters.py
+++ encounters.py
@@ -53,8 +53,8 @@ class RestEncounters(Encounters):
     def __init__(self, player_instance):
         super().__init__(player_instance)
     
-    @classmethod
-    def rest_encounter(self ,player_instance):
+    @staticmethod
+    def rest_encounter(player_instance):
         rest_chance = random.randint(0,3)
         if rest_chance == 0 or 1:
             player_instance.heal(5)
@@ -75,8 +75,8 @@ class EnemyEncounters(Encounters):
     def __init__(self, player_instance):
         super().__init__(player_instance)
     
-    @classmethod
-    def enemy_encounter(self, player_instance):
+    @staticmethod
+    def enemy_encounter(player_instance):
         enemy_chance = random.randint(0,4)
         if enemy_chance == 0 or 1:
             small_enemy = Enemy(name="Feral Imp", health=8, strength=1, intellect=3, attack_name="fireball")
@@ -99,8 +99,8 @@ class MysteryEncounters(Encounters):
     def __init__(self, player_instance):
         super().__init__(player_instance)
     
-    @classmethod
-    def mystery_encounter(self, player_instance):
+    @staticmethod
+    def mystery_encounter(player_instance):
         print("You Encounter A Strange Figure!")
         mystery_chance = random.randint(0,1)
         if mystery_chance == 0:
@@ -114,22 +114,22 @@ class SpecialEncounters(Encounters):
     def __init__(self, player_instance, step: int):
         super().__init__(player_instance, step)
 
-    @classmethod
-    def friendly_keep_visit(self, player_instance):
+    @staticmethod
+    def friendly_keep_visit(player_instance):
         print("You are welcomed to the Friendly Keep")
         print("You are fully rested and have a full stock of potions", end="\n\n")
         player_instance.heal(30)
         player_instance.potions = 5
 
-    @classmethod
-    def midway_boss(self, player_instance):
+    @staticmethod
+    def midway_boss(player_instance):
         wizard = Enemy(name="Cobolus The Wizard",health=15, strength=4, intellect=8, attack_name="Arcane Firestorm")
         print(f"You encounter {wizard.name}!")
         enemy_instance = wizard
         Combat.combat(player_instance, enemy_instance)
 
-    @classmethod
-    def enemy_keep_visit(self, player_instance):
+    @staticmethod
+    def enemy_keep_visit(player_instance):
         print("You must traverse Algolon's Keep!")
         sub_step = 0
         while sub_step < 10:
@@ -156,15 +156,15 @@ class SpecialEncounters(Encounters):
         enemy_instance = arch_mage
         Combat.combat(player_instance, enemy_instance)
 
-    @classmethod
-    def penultimate_boss(self, player_instance):
+    @staticmethod
+    def penultimate_boss(player_instance):
         great_wizard = Enemy(name="The Great Wizard Algolon", health=25, strength=5, intellect=12, attack_name="Cosmic Collision")
         print(f"You Battle {great_wizard.name}!")
         enemy_instance = great_wizard
         Combat.combat(player_instance, enemy_instance)
 
-    @classmethod
-    def final_boss(self, player_instance):
+    @staticmethod
+    def final_boss(player_instance):
         dragon = Enemy(name="Fortranus the Ancient One", health=40, strength=14, intellect=10, attack_name="Dragon Fire")
         print(f"You battle must now battle {dragon.name}!")
         enemy_instance = dragon
