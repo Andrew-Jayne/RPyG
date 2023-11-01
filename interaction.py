7299aa04f25e1ac6e3be5bb54d549101b9d09417
Andrew Jayne <majora158@gmail.com>
2023-11-01 19:43:56 -0400

    added extra exit condition

--- interaction.py
+++ interaction.py
@@ -65,13 +65,16 @@ class Interaction:
 
     @staticmethod
     def _sanitize(input_string):
+        #exit if string is longer that 128 Chars
+        if len(input_string) > 128:
+             print("Input Length Exceeds Expected Parameters: Exiting!")
+             exit()
+        #Set unwanted chars
         chars_to_remove = '!#*.[]{}\\|":;/<>\\\()\''
         control_chars = ''.join(map(chr, range(0, 32)))+ chr(127)
         literal_control_strings = ['\\n', '\\t', '\\r', '\\x0c', '\\x0b']  # Literal string representations of control chars
-
+        #Remove unwanted chars
         limited_string = input_string[:32] # Limit the input to 32 characters first
-
-        # Remove literal string representations of control characters
         for literal in literal_control_strings:
             limited_string = limited_string.replace(literal, '')
         cleaned_string = ''.join(char for char in limited_string if char not in chars_to_remove and char not in control_chars)
