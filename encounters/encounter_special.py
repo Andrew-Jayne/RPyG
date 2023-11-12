cc269f6272c4769a21195dde929a66f7045fd370
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2023-11-12 14:34:13 -0500

    moved all enemies into json files, improved enemy instance generation parsing

--- encounters/encounter_special.py
+++ encounters/encounter_special.py
@@ -1,4 +1,5 @@
 import random
+import json
 from actors.actor_enemy import Enemy
 from combat import Combat
 from actors.actor_follower import Follower
@@ -14,9 +15,8 @@ class SpecialEncounters():
 
     @staticmethod
     def midway_boss(player_instance):
-        wizard = Enemy(name="Cobolus The Wizard",health=15, strength=4, intellect=8, luck=6, attack_name="Arcane Firestorm")
-        print(f"You encounter {wizard.name}!")
-        enemy_instance = wizard
+        enemy_instance = __class__._get_special_enemy('midway_boss')
+        print(f"You encounter {enemy_instance.name}!")
         Combat.battle(player_instance, enemy_instance)
         if player_instance.strength >= 7 or player_instance.intellect >= 7:
             __class__._follower_joins(player_instance)
@@ -31,40 +31,50 @@ class SpecialEncounters():
             dungeon_chance = random.randint(0,5)
             match dungeon_chance:
                 case 0:
+                    print("You find a Store Room with some food & potions")
                     player_instance.potions += 2
                     player_instance.heal(2)
-                    print("You find a Store Room with some food & potions")
                 case 1:
                     sub_step += 2
                     print("You find a Secret Passage!")
                 case 4:
-                    mage = Enemy(name="Acolyte of Algolon", health=8, strength=3, intellect=7, luck=6, attack_name="Arcane Bolt")
-                    print(f"You encounter an {mage.name}!")
-                    enemy_instance = mage
+                    enemy_instance = __class__._get_special_enemy('keep_minion')
+                    print(f"You encounter an {enemy_instance.name}!")
                     Combat.battle(player_instance, enemy_instance)
             if player_instance.health == 0:
                 break
         print("At the end of the Keep you encounter Algolon's Arch Mage!")
-        arch_mage = Enemy(name="Algolon's Arch Mage", health=20, strength=4, intellect=10, luck=6, attack_name="Arcane Lightning")
-        enemy_instance = arch_mage
+        enemy_instance = __class__._get_special_enemy('keep_master')
         Combat.battle(player_instance, enemy_instance)
 
     @staticmethod
     def penultimate_boss(player_instance):
-        great_wizard = Enemy(name="The Great Wizard Algolon", health=25, strength=5, intellect=12, luck=6, attack_name="Cosmic Collision")
-        print(f"You Battle {great_wizard.name}!")
-        enemy_instance = great_wizard
+        enemy_instance = __class__._get_special_enemy('penultimate_boss')
+        print(f"You Battle {enemy_instance.name}!")
         Combat.battle(player_instance, enemy_instance)
 
     @staticmethod
     def final_boss(player_instance):
-        dragon = Enemy(name="Fortranus the Ancient One", health=40, strength=14, intellect=10, luck=6, attack_name="Dragon Fire")
-        print(f"You battle must now battle {dragon.name}!")
-        enemy_instance = dragon
+        enemy_instance = __class__._get_special_enemy('ultimate_boss')
+        print(f"You battle must now battle {enemy_instance.name}!")
         Combat.battle(player_instance, enemy_instance)
 
 
     ## Hidden Methods
+    def _get_special_enemy(enemy_indentiier):
+        with open('encounters/enemies_special.json', 'r') as file:
+            enemies_list = json.load(file)
+        enemy_attributes = enemies_list[enemy_indentiier]
+        special_enemy_instance = Enemy(
+            name=enemy_attributes['name'],
+            health=enemy_attributes['health'],
+            strength=enemy_attributes['strength'],
+            intellect=enemy_attributes['intellect'],
+            luck=enemy_attributes['luck'],
+            attack_name=enemy_attributes['attack_name']
+            )
+        return special_enemy_instance
+
     def _follower_joins(player_instance):
         
         mage_names = ["Grace","Torvalds","Knuth"]
@@ -77,7 +87,7 @@ class SpecialEncounters():
         elif player_instance.strength >= 7 and player_instance.strength > player_instance.intellect:
             follower_name_type = warrior_name
         elif player_instance.strength == player_instance.intellect:
-            follower_name_type = random.choice(mage_names,warrior_name)
+            follower_name_type = random.choice([mage_names,warrior_name])
 
         ## Set Follower Attributes (luck is handled in the class and is random, Follower also has gold and Potions using the same logic as player)
         if follower_name_type == mage_names:
