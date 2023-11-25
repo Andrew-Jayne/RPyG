995a94448f1ce6b5ca67a1e389938513fb84f773
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2023-11-25 17:36:27 -0500

    finished interaction refactor

---
+++ interaction/interaction_manual.py
@@ -0,0 +1,38 @@
+## Manual Interactions
+from interaction_utilities import validate_input
+
+def manual_enemy_encounter():
+    encounter_options = ["BATTLE", "FLEE"]
+    enounter_message = f"""
+Choose an Action:
+BATTLE
+FLEE
+
+"""
+    encounter_choice = validate_input(encounter_options, enounter_message)
+    return encounter_choice
+
+
+def manual_in_battle(player_name):
+    battle_options = ["ATTACK", "HEAL"]
+    battle_message = f"""
+{player_name}
+Choose an Action:
+ATTACK
+HEAL
+
+"""
+    battle_choice = validate_input(battle_options, battle_message)
+    return battle_choice
+    
+
+def manual_post_battle():
+    post_battle_options = ["HEAL", "TRAVEL", "SAVE"]
+    post_battle_message = """
+Choose an Action:
+HEAL
+TRAVEL
+
+"""     
+    post_battle_choice = validate_input(post_battle_options, post_battle_message)
+    return post_battle_choice
