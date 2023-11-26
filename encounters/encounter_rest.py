7db775e5069a6b5e764e898b672db49258b73cb4
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2023-11-26 18:20:17 -0500

    move rest encounters to json

--- encounters/encounter_rest.py
+++ encounters/encounter_rest.py
@@ -1,20 +1,21 @@
 import random
+import json
 from interaction.interaction import Interaction
-
-
 
 def rest_encounter(player_instance):
     rest_chance = random.randint(0,3)
+    with open('encounters/rest_common.json', 'r') as encounters_file:
+        encounters_lists = json.load(encounters_file)
     if rest_chance in range(0,2):
-        print("You Find a Tavern and Rest for a short time", end="\n\n")
-        player_instance.heal(5)
+        encounter_class = 'minor_rest'
     elif rest_chance == 2:
-        print("You Find an Inn and Rest for the evening", end="\n\n")
-        player_instance.heal(7)
+        encounter_class = 'medium_rest'
     elif rest_chance == 3:
-        print("""
-You Find A Vassal's Keep!
-You take a day to rest, and replenish your supplies.
-""", end="\n\n")
-        player_instance.heal(9)
+        encounter_class = 'large_rest'
+
+    encounter_sub_choice = random.randint(0, len(encounters_lists[encounter_class]) - 1)
+    print(f"{encounters_lists[encounter_class][encounter_sub_choice]['message']}", end="\n\n")
+    player_instance.heal(encounters_lists['minor_rest'][encounter_sub_choice]['recovery_amount'])
+
+    if encounter_class == 'large_rest':
         Interaction.at_merchant(player_instance)
