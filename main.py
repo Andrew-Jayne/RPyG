486a63fb6eb8d28e7719292520eaeece9adf6d54
Andrew Jayne <majora158@gmail.com>
2023-12-02 19:43:05 -0500

    added plans to main (I need a planning.md lol)

--- main.py
+++ main.py
@@ -51,3 +51,36 @@ Message.post_game_recap(player_instance)
 ### Move interaction functions into smaller sub classes
 
 ### IDEA Centralize json stores in story directory?
+
+
+### Gameplan: The Party Update
+# no more followers big sad
+
+# each game can have from 1-3 party memebers
+
+# can be warrior, mage, rogue, (1 of each or 2 + 1 or 3 + 0)
+
+# combat flow will be an option to pick a party memeber then their action.
+
+# enemies will attack in packs, this will allow for bosses to be powered up more
+
+# all exposed health & Damage values will be scaled by 10x, base player health will be 100 + 5 for each STR point
+
+# add randomness to damage values
+
+# Attack power will be set re balanced to be better based on attributes.
+
+#classes will be balanceed with a min of 5 in the chosen attribute but an max of 5 in the other
+# rogue's can will have 5 in AGL, but the other values are uncapped
+
+#attack order will be each party member, then each enemy
+#create extra comabt option, defend, protect, evade for each specilication
+
+## add damage type (pysical, magic,)
+## mages have resist to magic but weak to pysical, vise versa for warrior, rogue is balanced and has their resistances set based on attributes
+
+## update welcome.py to ask for party size, auto will do 3 always
+
+#strech goal: add AOE attack that can hit all enemy or player instances
+#strach goal: add leveling system based on battle actions
+#strech goal: add 2nd attack type for each class with new names (need to move to json)
