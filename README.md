21e0ec70963ec4cd03efb9ce75d43babfccb8527
Andrew Jayne <majora158@gmail.com>
2023-12-14 20:34:47 -0500

    added utils, moved allowed actions where they belong

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RPyG
 A Text based RPG in Python, mostly as an excuse to play with more advanced python/OOP concepts
+
+Run `main.py` in your terminal and have fun.
---
+++ _util/json2yaml.py
@@ -0,0 +1,29 @@
+
+def main():
+    import sys
+    import os
+    import json
+    import yaml
+
+    input_file = sys.argv[1] 
+
+    def change_file_extension(file_name, new_extension):
+        # Split the file name by '.' and replace the last part with the new extension
+        base = os.path.splitext(file_name)[0]
+        return base + new_extension
+
+    # Read JSON file
+    with open(input_file, 'r') as json_file:
+        data = json.load(json_file)
+
+    output_file  = change_file_extension(input_file, ".yaml")
+    # Convert and save as YAML
+    with open(output_file, 'w') as yaml_file:
+        yaml.dump(data, yaml_file)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
