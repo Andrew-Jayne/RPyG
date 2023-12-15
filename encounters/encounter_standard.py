508c0732c426add2313cd2284e04a934e91e15c2
Andrew-Jayne <273146696+Andrew-Jayne@users.noreply.github.com>
2023-12-14 21:30:24 -0500

    untested, but implemented  multi-event encounters

--- encounters/encounter_standard.py
+++ encounters/encounter_standard.py
@@ -2,50 +2,80 @@ import json
 import random
 from interaction.interaction import Interaction
 
+def find_encounter_by_id(full_item_list:list, target_item_id:str):
+        found_item = None
+        for active_item in full_item_list:
+              if active_item['id'] == target_item_id:
+                   found_item = active_item
+                   return found_item
+        if found_item == None:
+             print(f"Error Unable to Find an Event with the ID {target_item_id}")
+             exit()
+
+def execute_actor_action(event_object:object, target_instance_list:list):
+    print(event_object['message'])
+    for target in target_instance_list:
+        magnitude = int(event_object['magnitude'] / len(target_instance_list))
+        actor_method_name = event_object['actor_action']
+        actor_method_to_call = getattr(target, actor_method_name, None)
+        if callable(actor_method_to_call) == True:
+            actor_method_to_call(magnitude)
+        else:
+            print(f"Error Invalid Method Call: {actor_method_name}")
+            exit()
+
+def execute_special_action(event_object:object, target_instance_list:list):
+            # Run Actions for Encounter
+            special_method_name = event_object['special_action']
+            special_method_call = getattr(Interaction, special_method_name, None)
+            if callable(special_method_call) == True:
+                special_method_call(target_instance_list)
+            else:
+                print(f"error invalid special_action: {event_object['special_action']}")
+
 def standard_encounter(player_party_instance):
+
     allowed_actor_actions = [  "damage","heal","gain_gold","lose_gold","gain_potion","lose_potion","use_potion"]
     allowed_special_actions = ["at_merchant",]
 
     with open('encounters/standard_encounters.json', 'r') as encounters_file:
         encounters_objects_list = json.load(encounters_file)
-        active_encounter = random.choice(encounters_objects_list['events'])
+        current_event = random.choice(encounters_objects_list['events'])
 
     #Setup target for encounter action
-    match active_encounter['targets']:
-        case 'all':
-            targets = player_party_instance.members
-        case 'random':
-            targets = [random.choice(player_party_instance.members)]
-        case _:
-            print(f"invalid target: {active_encounter['targets']}")
-            exit()
+
 
     # validate actor_action
-    if active_encounter['actor_action'] in allowed_actor_actions:
-        # Run Actions for Encounter
-        print(active_encounter['message'])
-
-        for target in targets:
-            magnitude = int(active_encounter['magnitude'] / len(targets))
-            actor_method_name = active_encounter['actor_action']
-            actor_method_to_call = getattr(target, actor_method_name, None)
-            if callable(actor_method_to_call) == True:
-                actor_method_to_call(magnitude)
-            else:
-                print(f"Error Invalid Method Call: {actor_method_name}")
+    if current_event['actor_action'] in allowed_actor_actions:
+        match current_event['targets']:
+            case 'all':
+                targets = player_party_instance.members
+            case 'random':
+                targets = [random.choice(player_party_instance.members)]
+            case _:
+                print(f"invalid target: {current_event['targets']}")
                 exit()
+        execute_actor_action(current_event, targets)
     else:
-        print(f"Error Invalid Method Call: {active_encounter['actor_action']}")
+        print(f"Error Invalid Method Call: {current_event['actor_action']}")
+
+    # Run extra Actions if they exist
+    if current_event['additional_events'] != None:
+        for event_id in current_event['additional_events']:
+            new_event = find_encounter_by_id(encounters_objects_list['events'],event_id)
+            match new_event['targets']:
+                case 'all':
+                    targets = player_party_instance.members
+                case 'random':
+                    targets = [random.choice(player_party_instance.members)]
+                case _:
+                    print(f"invalid target: {current_event['targets']}")
+                    exit()
+            execute_actor_action(new_event, targets)
     
-    # Validate Speccial action
-    if active_encounter['special_action'] != None:
-        if active_encounter['special_action'] in allowed_special_actions:
-            # Run Actions for Encounter
-            special_method_name = active_encounter['special_action']
-            special_method_call = getattr(Interaction, special_method_name, None)
-            if callable(actor_method_to_call) == True:
-                special_method_call(player_party_instance)
-            else:
-                print(f"error invalid special_action: {active_encounter['special_action']}")
+    # Validate Special action
+    if current_event['special_action'] != None:
+        if current_event['special_action'] in allowed_special_actions:
+            execute_special_action(current_event,player_party_instance)
         else:
-            print(f"error invalid special_action: {active_encounter['special_action']}")
+            print(f"error invalid special_action: {current_event['special_action']}")
