ebe3927dc23496eabb5718398cebaadc184e16de
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2024-01-10 02:24:47 -0500

    more stuff

--- logging/logging.py
+++ logging/logging.py
@@ -1,3 +1,5 @@
+from interaction.interaction_utilities import sanitize
+
 def write_log(message:str):
     with open('log.txt', 'a') as log_file:
-        log_file.write(message)
+        log_file.write(sanitize(message))
