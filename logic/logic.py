5c744bc64a06599e7cafa30303086cabada93e52
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2024-01-25 22:06:30 -0500

    dry-ed out combat logic a bit

---
+++ logic/logic.py
@@ -0,0 +1,30 @@
+import random
+
+def select_combat_target(target_party_instance) -> int:
+    """
+    Takes a full party instance, and returns the index of the target member in the members array/list as an int
+    """
+    target_party_members = target_party_instance.members
+    method_id = random.choice(["MAX_ATK","MIN_HP","RANDOM"])
+    match method_id:
+        case "MAX_ATK":
+            target_attributes_list = []
+            for index,member in enumerate(target_party_members):
+                target_attributes = (index,member.attack_power)
+                target_attributes_list.append(target_attributes)
+            sorted_target_attributes_list = sorted(target_attributes_list, key=lambda x: x[1], reverse=True)
+            return sorted_target_attributes_list[0][0]
+        
+        case "MIN_HP":
+            target_attributes_list = []
+            for index,member in enumerate(target_party_members):
+                target_attributes = (index,member.health)
+                target_attributes_list.append(target_attributes)
+            sorted_target_attributes_list = sorted(target_attributes_list, key=lambda x: x[1])
+            return sorted_target_attributes_list[0][0]
+        
+        case "RANDOM":
+            return random.randint(0,(len(target_party_members) - 1))
+        case _:
+            print("Big Problem in Select_Target, Go buy a lotto ticket")
+            exit()
