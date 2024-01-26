034b377f200f4d508925daa0712d62ac22aef221
Andrew Jayne <majora158@gmail.com>
2024-01-25 21:54:49 -0500

    found another "you"

--- interaction/interaction_automatic.py
+++ interaction/interaction_automatic.py
@@ -10,7 +10,7 @@ def auto_in_battle(player_instance):
     if player_instance.health <= 4 and player_instance.potions != 0:
         return "HEAL" 
     elif player_instance.potions == 0:
-            print("You have no remaining potions and must make a stand!")
+            print(f"{player_instance.name} has no remaining potions and must make a stand!")
             return "ATTACK"
     else:
             return random.choice(["EVADE","ATTACK","ATTACK","ATTACK"])
