e37d8075e7286d39bc90c90bb04c74548ba086cc
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2024-01-30 08:43:00 -0500

    unborked combat, still trying to find the source of the infinte gold exploit

--- combat/combat.py
+++ combat/combat.py
@@ -17,14 +17,26 @@ class Combat:
         battle_complete = False
         while battle_complete == False:
             Message.battle_hud_message(player_party_instance, enemy_party_instance)
+
             # Players Attack
+            if len(enemy_party_instance.members) == 0:
+                # End Battle if all Enemies are dead
+                battle_complete = True
+                break
+
             if battle_complete == False:
                 for player_instance in player_party_instance.members:
+
+                    ## Evalute if members are poisioned and damage accordingly
                     if player_instance.is_poisioned == True:
                         Message.poison_damage_message(player_instance)
                         player_instance.damage(player_instance.poison_damage)
 
-                    if len(enemy_party_instance.members) != 0:
+                    if len(enemy_party_instance.members) == 0:
+                        # End Battle if all Enemies are dead
+                        battle_complete = True
+                        break
+                    else:
                         match Interaction.in_battle(player_instance):
                             case "ATTACK": #select target
                                 target_index = int(Interaction.choose_combat_target(enemy_party_instance))
@@ -39,44 +51,47 @@ class Combat:
                                 player_instance.will_evade = True
                             case "HEAL":
                                 player_instance.use_potion()
-                    else:
-                        battle_complete = True
-            else:
-                battle_complete = True
+                        
 
             # Enemies Attack
-            if  battle_complete == False:
-                if len(player_party_instance.members) != 0:
+            if battle_complete == False:
+                if len(player_party_instance.members) == 0:
+                    # End Battle if All Players are dead
+                    battle_complete = True
+                    break
+                else:
                     for enemy_instance in enemy_party_instance.members:
+
+                        ## Evalute if members are poisioned and damage accordingly
                         if enemy_instance.is_poisioned == True:
                             Message.poison_damage_message(enemy_instance)
                             enemy_instance.damage(enemy_instance.poison_damage)
-                            if len(player_party_instance.members) != 0:
-                                target_index = select_combat_target(player_party_instance)
-                                target_player = player_party_instance.members[target_index]
-                                if target_player.will_evade == True:
-                                    if evade(target_player) == False:
-                                        Message.evade_failure_message(player_instance.name)
-                                        attack(attacker_instance=enemy_instance, target_instance=target_player)
-                                        target_player.will_evade = False
-                                    else:
-                                        Message.evade_success_message(player_instance.name)
-                                        target_player.will_evade = False
-                                else:
-                                    attack(attacker_instance=enemy_instance, target_instance=target_player)
-                                if target_player.health == 0:
-                                    Message.defeated_message(target_player.name)
-                                    player_party_instance.lose_member(target_player)
-                                    
-                            else:
-                                battle_complete = True
-                else:
-                    battle_complete = True
-                ## End Battle if Everyone dies
-            else:
-                battle_complete = True
 
-        ## Display Victory Message if player does not die
+                        if len(player_party_instance.members) == 0:
+                            # End Battle if All Players are dead
+                            battle_complete = True
+                            break
+
+                        else:
+                            target_index = select_combat_target(player_party_instance)
+                            target_player = player_party_instance.members[target_index]
+
+                            if target_player.will_evade == True:
+                                if evade(target_player) == True:
+                                    Message.evade_success_message(player_instance.name)
+                                    target_player.will_evade = False
+                                else:
+                                    Message.evade_failure_message(player_instance.name)
+                                    attack(attacker_instance=enemy_instance, target_instance=target_player)
+                                    target_player.will_evade = False   
+                            else:
+                                attack(attacker_instance=enemy_instance, target_instance=target_player)
+
+                            if target_player.health == 0:
+                                Message.defeated_message(target_player.name)
+                                player_party_instance.lose_member(target_player)    
+
+        ## Display Victory Message if players do not die
         if len(player_party_instance.members) != 0 and len(enemy_party_instance.members) == 0 and battle_complete == True:
             Message.defeated_message(enemy_party_instance.name)
             post_battle(player_party_instance)
