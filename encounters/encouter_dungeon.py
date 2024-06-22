22ef343fa1d922e408f10af626f1e7f60af9c6fb
Andrew-Jayne <273146696+Andrew-Jayne@users.noreply.github.com>
2024-06-22 01:14:20 -0400

    what I did on the plane

---
+++ encounters/encouter_dungeon.py
@@ -0,0 +1,65 @@
+import random
+import json
+import copy
+from message.message import Message
+from actors.actor_party import EnemyParty
+from combat.combat import Combat
+from message.message import Message
+
+# Create a Dungeon Object?
+# Dungeon is a Funciton?
+
+
+
+
+
+class Dungeon():
+    def __init__(self,dungeon_id) -> None:
+
+        json.load()
+        pass 
+
+
+# Just for Type Checking
+from actors.actor_party import PlayerParty
+
+def enemy_keep_visit(player_party_instance:PlayerParty) -> None:
+    if not isinstance(player_party_instance, PlayerParty):
+        raise ValueError("The 'player_party_instance' parameter must be of type PlayerParty. Received type: {}".format(type(player_party_instance).__name__))
+    sub_step = 0
+    while sub_step < 10:
+        sub_step += 1
+        Message.display_message(f"{sub_step}", 1)
+        dungeon_chance = random.randint(0,5)
+        match dungeon_chance:
+            case 0:
+                Message.display_message("Your Party finds a Store Room with some food & potions", 1)
+                for member_instance in player_party_instance.members:
+                    member_instance.gain_potion(2)
+                    member_instance.heal(20)
+            case 1:
+                sub_step += 2
+                Message.display_message("Your Party finds a Secret Passage!", 1)
+            case 4:
+                enemy_instance = __class__._get_special_enemy('keep_minion') ## Need to move this to a Party
+                Message.display_message(f"Your Party encounter a group of {enemy_instance.name}s!", 1)
+                enemy_count = int(len(player_party_instance.members) + random.randint(-2,2))
+                if enemy_count == 0:
+                    enemy_count = 1
+
+                enemy_party_instances = []
+                for _ in range(0,enemy_count):
+                    enemy_party_instances.append(copy.deepcopy(enemy_instance))
+                enemy_party = EnemyParty(f"group of {enemy_count} {enemy_instance.name}" ,enemy_party_instances)
+                Combat.battle(player_party_instance, enemy_party)
+        if len(player_party_instance.members) == 0:
+            break
+    if len(player_party_instance.members) != 0:
+        Message.display_message("At the end of the Keep Your Party encounters Algolon's Arch Mage!", 1)
+        enemy_instance = __class__._get_special_enemy('keep_master')
+        enemy_party = EnemyParty(enemy_instance.name, [enemy_instance])
+        Combat.battle(player_party_instance, enemy_party)
+        if len(player_party_instance.members) != 0:
+            Message.special_encounter_message(player_party_instance.progress, player_party_instance.name,"success_messages")
+        else:
+            Message.special_encounter_message(player_party_instance.progress, player_party_instance.name,"failure_messages")
