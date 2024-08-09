427397757494f0cadc609104afe93cdbd24b767e
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2024-08-09 17:52:54 -0600

    holy shit the linter is mean

---
+++ utilites/utilities.py
@@ -0,0 +1,8 @@
+# type_utils.py
+def ensure_type(instance: object, expected_type: type, variable_name: str):
+    if not isinstance(instance, expected_type):
+        raise ValueError(f"""
+The '{variable_name}' parameter must be of type {expected_type.__name__}.
+Received type: {type(instance).__name__}.
+"""
+                         )
