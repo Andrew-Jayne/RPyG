cd1fd18c7b0fb2735395146440c4b89dffcea312
Andrew-Jayne <77167778+Andrew-Jayne@users.noreply.github.com>
2024-08-12 11:15:02 -0600

    finished display & combat actions

--- display/display.py
+++ display/display.py
@@ -1,12 +1,14 @@
 import os
 from interaction.interaction import Interaction
 
+
 class Display:
     # Utilities
     @staticmethod
     def clear_display() -> None:
         "If the game is not in Auto Mode, will clear the display"
-        ## Screen is not cleared in Auto mode since It's better for testing and Auto mode is kinda turning into a debug mode (I might make that an option at some point)
+        # Screen is not cleared in Auto mode since It's better for testing
+        # Auto mode is kinda turning into a debug mode (I might make that an option at some point)
         if Interaction.global_game_mode == "MANUAL":
             # For Windows
             if os.name == 'nt':
