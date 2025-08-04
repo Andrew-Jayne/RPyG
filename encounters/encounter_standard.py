53ba85a1ad30bef49a6f289ccee43a0b46ba9ae8
x86jawa <x86jawa@sandcrawler.local>
2025-08-04 22:39:17 +0200

    full content library is working, just need to deal with the dungeon, specials and story content

--- encounters/encounter_standard.py
+++ encounters/encounter_standard.py
@@ -1,8 +1,15 @@
 import random
-from typing import Any
 
 from actors import PlayableActor, PlayerParty
 from content.content import ENCOUNTERS_STANDARD
+from content.encounter_library import (
+    ActorAction,
+    Encounter,
+    EncounterLibrary,
+    EncounterTarget,
+    EncounterType,
+    SpecialAction,
+)
 from interaction.interaction import Interaction
 from message.message import Message
 from utilites import ensure_type
@@ -10,15 +17,15 @@ from utilites import ensure_type
 
 @staticmethod
 def set_encounter_targets(
-    current_event_targets: str, player_party_instance: PlayerParty
+    current_event_targets: EncounterTarget, player_party_instance: PlayerParty
 ):
-    ensure_type(current_event_targets, str, "current_event_targets")
+    ensure_type(current_event_targets, EncounterTarget, "current_event_targets")
     ensure_type(player_party_instance, PlayerParty, "player_party_instance")
 
     match current_event_targets:
-        case "all":
+        case EncounterTarget.ALL:
             return player_party_instance.members
-        case "random":
+        case EncounterTarget.RANDOM:
             return [random.choice(player_party_instance.members)]
         case _:
             raise ValueError(f"invalid target: {current_event_targets}")
@@ -26,14 +33,14 @@ def set_encounter_targets(
 
 @staticmethod
 def find_encounter_by_id(
-    encounters_dict: dict[str, object],
+    encounter_library: EncounterLibrary,
     target_item_id: str,
-) -> object:
-    ensure_type(encounters_dict, dict, "encounters_dict")
+) -> Encounter:
+    ensure_type(encounter_library, EncounterLibrary, "encounters_library")
     ensure_type(target_item_id, str, "target_item_id")
 
     # Directly access the event by its ID
-    found_item = encounters_dict.get(target_item_id)
+    found_item = encounter_library.encounters.get(target_item_id)
     if found_item is None:
         raise FileNotFoundError(
             f"Error: Unable to find an event with the ID {target_item_id}"
@@ -43,131 +50,130 @@ def find_encounter_by_id(
 
 @staticmethod
 def execute_actor_action(
-    event_object: dict[str, Any],
+    encounter: Encounter,
     target_instance_list: list[PlayableActor],
 ) -> None:
-    ensure_type(event_object, dict, "event_object")
+    ensure_type(encounter, Encounter, "encounter")
     ensure_type(target_instance_list, list, "target_instance_list")
+    for actor in target_instance_list:
+        ensure_type(actor, PlayableActor, "actor")
 
-    magnitude = int(event_object["magnitude"] / len(target_instance_list))
-    Message.display_message(event_object["message"], 1)
+    magnitude = int(encounter.magnitude / len(target_instance_list))
+    Message.display_message(encounter.message, 1)
 
     for target in target_instance_list:
-        match event_object["actor_action"]:
-            case "damage":
+        match encounter.actor_action:
+            case ActorAction.DAMAGE:
                 target.damage(magnitude)
-            case "heal":
+            case ActorAction.HEAL:
                 target.heal(magnitude)
-            case "gain_gold":
+            case ActorAction.GAIN_GOLD:
                 target.inventory.gain_gold(magnitude)
-            case "lose_gold":
+            case ActorAction.LOSE_GOLD:
                 target.inventory.lose_gold(magnitude)
-            case "gain_potion":
+            case ActorAction.GAIN_POTION:
                 target.inventory.gain_potion(magnitude)
-            case "lose_potion":
+            case ActorAction.LOSE_POTION:
                 target.inventory.lose_potion(magnitude)
-            case "use_potion":
-                target.use_potion()
             case _:
-                raise ValueError(
-                    f"Error Invalid Method Call: {event_object['actor_action']}"
-                )
+                raise ValueError(f"Error Invalid Method Call: {encounter.actor_action}")
 
 
 @staticmethod
-def execute_special_action(
-    event_object: dict[str, object], player_party_instance: PlayerParty
-):
-    ensure_type(event_object, dict, "event_object")
+def execute_special_action(encounter: Encounter, player_party_instance: PlayerParty):
+    ensure_type(encounter, Encounter, "event_object")
     ensure_type(player_party_instance, PlayerParty, "player_party_instance")
-    match event_object["special_action"]:
-        case "at_merchant":
+    match encounter.special_action:
+        case SpecialAction.AT_MERCHANT:
             Interaction.at_merchant(player_party_instance)
         case _:
             raise ValueError(
-                f"error invalid special_action: {event_object['special_action']}"
+                f"error invalid special_action: {encounter.special_action}"
             )
 
 
 @staticmethod
 def run_extra_actions(
-    event_object: Any,
+    encounter: Encounter,
     player_party_instance: PlayerParty,
-    encounter_objects: dict[str, Any],
+    encounter_library: EncounterLibrary,
 ) -> None:
+    ensure_type(encounter, Encounter, "encounter")
     ensure_type(player_party_instance, PlayerParty, "player_party_instance")
-    ensure_type(encounter_objects, dict, "encounter_objects_dict")
+    ensure_type(encounter_library, EncounterLibrary, "encounter_library")
 
     # Run extra Actions if they exist
-    if event_object["additional_events"] is not None:
-        for event_id in event_object["additional_events"]:
-            new_event: dict = find_encounter_by_id(encounter_objects, event_id)
-            targets = set_encounter_targets(new_event["targets"], player_party_instance)
+    if encounter.additional_events is not None:
+        for event_id in encounter.additional_events:
+            new_event: Encounter = find_encounter_by_id(encounter_library, event_id)
+            targets = set_encounter_targets(new_event.targets, player_party_instance)
             execute_actor_action(new_event, targets)
 
     # Run Special Actions if they exist
-    if event_object["special_action"] is not None:
-        execute_special_action(event_object, player_party_instance)
+    if encounter.special_action is not None:
+        execute_special_action(encounter, player_party_instance)
 
 
 @staticmethod
 def standard_encounter(player_party_instance: PlayerParty) -> None:
     ensure_type(player_party_instance, PlayerParty, "player_party_instance")
 
-    current_event = ENCOUNTERS_STANDARD[random.choice(list(ENCOUNTERS_STANDARD.keys()))]
+    current_event = random.choice(list(ENCOUNTERS_STANDARD.encounters.values()))
 
-    targets = set_encounter_targets(current_event["targets"], player_party_instance)
+    targets = set_encounter_targets(current_event.targets, player_party_instance)
 
-    match current_event["encounter_type"]:
-        case "REST":
-            Message.display_message(current_event["pre_message"], 1)
+    match current_event.encounter_type:
+        case EncounterType.REST:
+            Message.display_message(current_event.pre_message, 1)
             if Interaction.confirm_rest() is True:
                 execute_actor_action(current_event, targets)
                 run_extra_actions(
                     current_event, player_party_instance, ENCOUNTERS_STANDARD
                 )
-                Message.display_message(current_event["post_message"], 1)
+                Message.display_message(current_event.post_message, 1)
             else:
                 Message.display_message("They Travel onwards", 1)
 
-        case "MYSTERY":
+        case EncounterType.MYSTERY:
             # this is pretty brittle right now, can be reworked later
-            Message.display_message(current_event["pre_message"], 1)
+            Message.display_message(current_event.pre_message, 1)
             match Interaction.mystery_action():
                 case "GREET":
                     execute_actor_action(current_event, targets)
                     run_extra_actions(
                         current_event, player_party_instance, ENCOUNTERS_STANDARD
                     )
-                    Message.display_message(current_event["post_message"], 1)
+                    Message.display_message(current_event.post_message, 1)
                 case "ATTACK":
                     # if you attack you get attacked
-                    static_event: dict = find_encounter_by_id(
+                    static_event: Encounter = find_encounter_by_id(
                         ENCOUNTERS_STANDARD, "surprise_attack"
                     )
                     execute_actor_action(static_event, targets)
                     run_extra_actions(
                         current_event, player_party_instance, ENCOUNTERS_STANDARD
                     )
-                    Message.display_message(static_event["post_message"], 1)
+                    Message.display_message(static_event.post_message, 1)
                 case _:
                     raise ValueError("Null Action Set MonkaS")
 
-        case "LOOT":
-            Message.display_message(current_event["pre_message"], 1)
+        case EncounterType.LOOT:
+            Message.display_message(current_event.pre_message, 1)
             match Interaction.loot_action():
                 case "OPEN":
                     execute_actor_action(current_event, targets)
                     run_extra_actions(
-                        current_event, player_party_instance, ENCOUNTERS_STANDARD
+                        current_event,
+                        player_party_instance,
+                        ENCOUNTERS_STANDARD,
                     )
-                    Message.display_message(current_event["post_message"], 1)
+                    Message.display_message(current_event.post_message, 1)
                 case "LEAVE":
                     Message.display_message("You leave the chest undisturbed", 1)
                 case _:
                     raise ValueError("Null Action Set MonkaS")
 
-        case "NONE":
+        case EncounterType.NONE:
             pass
         case _:
             raise ValueError(
