80428e5fa9beab343a3ed55d1e52632802098685
Andrew-Jayne <273146696+Andrew-Jayne@users.noreply.github.com>
2026-02-12 01:22:03 +0100

    accept bullying

--- RPyG/utilities/__init__.py
+++ RPyG/utilities/__init__.py
@@ -10,11 +10,9 @@ def ensure_type(
     variable_name: str,
 ) -> None:
     if isinstance(instance, expected_type) is False:
-        raise ValueError(
-            f"""
-The '{variable_name}' parameter must be of type {expected_type.__name__}.
-Received type: {type(instance).__name__}.
-"""
+        raise TypeError(
+            f"""The '{variable_name}' parameter must be of type {expected_type.__name__}.
+Received type: {type(instance).__name__}."""
         )
 
 
