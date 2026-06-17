65069df3626b7a8c8b387d2e9af385758e26a88c
Andrew Jayne <273146696+Andrew-Jayne@users.noreply.github.com>
2026-06-16 20:14:15 -0600

    remove pickle gamestate handler (because pickle ACE)

--- interfaces/interface_components/game_state_sources/__init__.py
+++ interfaces/interface_components/game_state_sources/__init__.py
@@ -4,13 +4,9 @@ from interfaces.interface_components.game_state_sources.abstract_game_state_hand
 from interfaces.interface_components.game_state_sources.json_game_state_handler import (
     JsonGameStateHandler,
 )
-from interfaces.interface_components.game_state_sources.pickle_game_state_handler import (
-    PickleGameStateHandler,
-)
 
 
 __all__ = [
     "GameStateHandler",
     "JsonGameStateHandler",
-    "PickleGameStateHandler",
 ]
